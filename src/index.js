a9fb08a52eb262bc747a2d1cf8616ec095481eae
Denis <mr.denisov1995@yandex.ru>
2020-02-24 20:49:24 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function toReadable (number) {
-  
+const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const tenths = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+const round_tenths = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+if (number >=0 && number <10) {
+    return units[number];
+}
+if (number >= 10 && number < 20) {
+    return tenths[number - 10];
+}
 }
